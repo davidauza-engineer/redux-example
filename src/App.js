284c3ae7bc88425090b39e5ad6fb026499f9b1d7
David Auza <davidauza01@gmail.com>
2021-08-25 16:49:40 -0500

    Implement FETCH_POSTS action

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Posts from './components/Posts';
 import PostForm from './components/Postform';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
